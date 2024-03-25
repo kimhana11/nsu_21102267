3a784df1ad39ae604361a6b551779216223596cb
kimhana11 <117277958+kimhana11@users.noreply.github.com>
2024-03-25 11:23:12 +0900

    Add files via upload

---
+++ 도전과제/chap03/FactorDemo2.java
@@ -0,0 +1,29 @@
+package hw3;
+import java.util.Scanner;
+public class FactorDemo2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+            System.out.print("팩토리얼 값을 구할 정수: ");
+            int n = sc.nextInt();
+
+            if (n < 0) {
+                System.out.println("음수를 입력받았습니다. 프로그램을 종료합니다.");
+            } else {
+                int result = factorial(n);
+                System.out.println(result);
+            }
+
+        sc.close();
+        }
+
+    public static int factorial(int x) {
+        int result = 1;
+        while (x > 0) {
+            result *= x;
+            x--;
+        }
+        return result;
+    }
+}
