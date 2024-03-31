b310ebfa7db02e3afaaaac29ec2729c3d2dd0caf
kimhana11 <117277958+kimhana11@users.noreply.github.com>
2024-03-31 22:46:27 +0900

    Add files via upload

---
+++ 실습/chap04/CircleDemo2.java
@@ -0,0 +1,16 @@
+package ch4;
+class Circle{
+	private double radius;
+	
+	public Circle(double r) {
+		radius = r;
+	}
+}
+public class CircleDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle myCircle = new Circle(10.0);
+	}
+
+}
