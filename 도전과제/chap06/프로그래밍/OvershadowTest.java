7716e3b3869cfa321177289b8cbe0a29264a412c
kimhana11 <117277958+kimhana11@users.noreply.github.com>
2024-06-04 22:53:17 +0900

    Delete 도전과제/chap06/프로그래밍 directory

--- 도전과제/chap06/프로그래밍/OvershadowTest.java
+++
@@ -1,9 +0,0 @@
-package challenge_06_programming;
-
-public class OvershadowTest {
-    public static void main(String[] args) {
-        Parent p = new Child();
-        System.out.println(p.name);
-        p.print();
-    }
-}
--- 도전과제/chap06/프로그래밍/Parent.java
+++
@@ -1,9 +0,0 @@
-package challenge_06_programming;
-
-public class Parent {
-	String name = "영조";
-
-	void print() {
-		System.out.println("나는 조선의 제21대 국왕이다.");
-	}
-}
